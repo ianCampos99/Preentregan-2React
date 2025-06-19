e19428966eba9b1db771b2fba9dd65d0aa1e2ae5
Ian <iancampos025@gmail.com>
2025-06-19 12:42:16 -0300

    carga final nuevo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,36 +5,44 @@ import CartWidget from './CartWidget';
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light px-4">
-            <div className="container-fluid">
-                <Link className="navbar-brand fw-bold" to="/">Tienda Fluxx</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+            <Link className="navbar-brand" to="/">Tienda Fluxx</Link>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Inicio</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/category/electronics">Electrónica</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/category/jewelery">Joyería</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/category/men's clothing">Hombres</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/category/women's clothing">Mujeres</Link>
-                        </li>
-                    </ul>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarContent"
+                aria-controls="navbarContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-                    <CartWidget />
-                </div>
+            <div className="collapse navbar-collapse" id="navbarContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Inicio</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/category/electronics">Electrónica</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/category/jewelery">Joyería</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/category/men's clothing">Hombre</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/category/women's clothing">Mujer</Link>
+                    </li>
+                </ul>
+
+                <CartWidget />
             </div>
         </nav>
     );
 };
 
 export default NavBar;
+
